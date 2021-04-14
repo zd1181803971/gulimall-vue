ff1c6f74319c69c23ba3f860d1d77257aef49158
@zhao_dong1181803971 <zd990826>
2021-04-14 23:49:23 +0800

    init

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        console.log(data)
+      })
+    }
+  },
+  created () {
+    this.getMenus()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
